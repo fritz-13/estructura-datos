2a2631e60b84d22948bedea081e909348deee960
fritz-13 <fritz_tlv@hotmail.com>
2026-03-27 14:57:22 -0500

    Fix estructura CSS y JS

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,7 @@
 const btnMenu = document.getElementById('btn-menu');
 const nav = document.getElementById('nav');
-const submenus = document.querySelectorAll('.submenu');
 
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle('mostrar');
 });
 
-// activar submenus en móvil
-submenus.forEach(menu => {
-    menu.addEventListener('click', () => {
-        menu.classList.toggle('activo');
-    });
-});
